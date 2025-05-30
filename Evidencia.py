8eaf8f7485fa0762155bba8284533ae5a44ecae4
jahuey <fjahuey@uach.mx>
2025-05-29 18:08:38 -0600

    Update Evidencia.py

--- Evidencia.py
+++ Evidencia.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title(":blue[Asistente virtual para el curso de Mejoramiento Animal] 🐎🐂🐷🐐🐑🐔")
+st.title(":blue[Asistente virtual para el curso de Mejoramiento Animal] 🐎 🐂 🐷 🐐 🐑 🐔")
 
 st.image("https://cdn.slidesharecdn.com/ss_thumbnails/mejoramientogeneticoanimal-240418190359-8edceafb-thumbnail.jpg?width=560&fit=bounds")
 
