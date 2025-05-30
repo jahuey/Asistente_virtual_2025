a68116056677ddd57c9f9bfca5b78c41ddb398e1
jahuey <fjahuey@uach.mx>
2025-05-30 12:46:56 -0600

    Update Asistente_MA.py

--- Asistente_MA.py
+++ Asistente_MA.py
@@ -111,3 +111,31 @@ if st.session_state.tema_seleccionado:
     else:
         st.info("Aún no hay razonados cargados para este tema.")
 
+
+openai_api_key = st.secrets["api_key"] 
+# Create an OpenAI client.
+client = OpenAI(api_key=openai_api_key)
+
+prompt = st.chat_input("What is up?")
+if prompt==None:
+   st.stop()
+
+with st.chat_message("user",avatar = "🦖"):
+   st.markdown(prompt)
+
+# Generate a response using the OpenAI API.
+
+stream = client.chat.completions.create(
+        model="gpt-4o-mini",  
+        messages=[
+            {"role": "system", "content": "You are an assistant."},
+            {"role": "user", "content": prompt}
+        ],
+        max_tokens=800,
+        temperature=0,
+    )
+respuesta = stream.choices[0].message.content
+
+with st.chat_message("assistant"):
+   st.write(respuesta)
+
