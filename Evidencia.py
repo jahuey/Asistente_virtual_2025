436d5d7eee4fbed995787b322d9a554726bfabd1
jahuey <fjahuey@uach.mx>
2025-05-29 18:02:39 -0600

    Update Evidencia.py

--- Evidencia.py
+++ Evidencia.py
@@ -1,5 +1,9 @@
 import streamlit as st
 
-st.title("Asistente virtual para el curso de Mejoramiento Animal")
+st.title("blue[Asistente virtual para el curso de Mejoramiento Animal] 🐎")
 
 st.image("https://cdn.slidesharecdn.com/ss_thumbnails/mejoramientogeneticoanimal-240418190359-8edceafb-thumbnail.jpg?width=560&fit=bounds")
+
+nombre = st.text_input("¿Cuál es tu nombre?")
+if nombre:
+    st.write(f"Hola, {nombre} 👋, bienvenido al curso de Mejoramiento Animal,")
